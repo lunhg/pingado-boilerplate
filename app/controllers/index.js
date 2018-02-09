f6d9c4336aac73b911c20e51dc46e230551b6ba7
lunhg <lunhg@users.noreply.github.com>
2018-02-09 17:01:47 -0200

    fixed some path, but the test will fail

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,6 @@ module.exports = function(req, res){
     res.cookie('pingado-lang', 'en', {timeout:90000000, httpOnly:true})
     res.render('index', {
 	title: res.__('PINGADO_-_WEB_APPLICATION_FRAMEWORK'),
-	_csrf: res.csrfToken()
+	_csrf: req.csrfToken()
     })
 }
